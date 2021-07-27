d01c5c0c253d9685032c32e7ed00c2600bf79723
Talip-Sfeir <talip.cesur@gmail.com>
2021-07-27 16:00:29 +0200

    Section 4 to Section 4.19

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,7 +1,62 @@
 const chalk = require('chalk');
+const yargs = require('yargs');
+const notes = require('./notes.js');
 
-const getNotes = require('./notes.js');
+yargs.version('1.1.0');
 
-const result = getNotes();
+// Create add command
 
-console.log(chalk.yellow.bold(result));
+yargs.command({
+	command: 'add',
+	describe: 'Add a note',
+	builder: {
+		title: {
+			describe: 'Note title',
+			demandOption: true,
+			type: 'string',
+		},
+		body: {
+			describe: 'Note body',
+			demandOption: true,
+			type: 'string',
+		},
+	},
+	handler: (argv) => {
+		console.log('Adding a new note :');
+		notes.addNote(argv.title, argv.body);
+	},
+});
+
+// Create remove command
+
+yargs.command({
+	command: 'remove',
+	describe: 'Remove a note',
+	handler: (argv) => {
+		console.log('Removing a note');
+	},
+});
+
+// Create list command
+
+yargs.command({
+	command: 'list',
+	describe: 'List out all note',
+	handler: (argv) => {
+		console.log('Listing out all note');
+	},
+});
+
+// Create read command
+
+yargs.command({
+	command: 'read',
+	describe: 'Read a note',
+	handler: (argv) => {
+		console.log('Reading a note');
+	},
+});
+
+// add, remove, read, list
+
+yargs.parse();
