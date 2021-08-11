00a235c0729a02aea396aa0a14b72ec81a4165ab
talipcr <talip.cesur@gmail.com>
2021-08-11 11:20:36 +0200

    Section 10.11 to Section 10.12

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -14,6 +14,22 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (err, client) => {
 	const User = db.collection('users');
 	const Task = db.collection('tasks');
 
+	// User.deleteMany({ username: 'test' })
+	// 	.then(() => {
+	// 		console.log('Users deleted');
+	// 	})
+	// 	.catch((err) => {
+	// 		console.log(err);
+	// 	});
+
+	Task.deleteOne({ description: 'test1' })
+		.then(() => {
+			console.log('Task deleted');
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+
 	// User.updateOne(
 	// 	{ _id: new ObjectId('61128108c8e7277778de2d68') },
 	// 	{
@@ -29,18 +45,18 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (err, client) => {
 	// 		console.log(err);
 	// 	});
 
-	Task.updateMany(
-		{},
-		{
-			$set: {
-				completed: true,
-			},
-		}
-	)
-		.then(() => {
-			console.log('Task updated');
-		})
-		.catch((err) => {
-			console.log(err);
-		});
+	// Task.updateMany(
+	// 	{},
+	// 	{
+	// 		$set: {
+	// 			completed: true,
+	// 		},
+	// 	}
+	// )
+	// 	.then(() => {
+	// 		console.log('Task updated');
+	// 	})
+	// 	.catch((err) => {
+	// 		console.log(err);
+	// 	});
 });
