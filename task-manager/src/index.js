0fc2898758db64724bbdbc228f79c71d26c9cb22
talipcr <talip.cesur@gmail.com>
2021-10-06 17:03:11 +0200

    Section 15.8 to Section 16.10

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,14 +1,6 @@
-const express = require('express');
-require('./db/mongoose');
-const userRouter = require('./routers/user');
-const taskRouter = require('./routers/task');
+const app = require('./app.js');
 
-const app = express();
 const port = process.env.PORT;
-
-app.use(express.json());
-app.use(userRouter);
-app.use(taskRouter);
 
 app.route('/').get((req, res) => {
 	res.send('Welcome to Task App!');
